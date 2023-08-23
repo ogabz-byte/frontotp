8e7f2f6334ba629018425cbe134b7af3298675df
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2023-08-23 06:11:28 -0700

    Update Headers.js

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -9,10 +9,10 @@ const Headers = () => {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <NavLink to="/" className=" text-light text-decoration-none">HpCloud</NavLink>
+          <NavLink to="/" className=" text-light text-decoration-none">OGABz</NavLink>
           <Nav className="">
             <NavLink to="/register" className="mt-3 mx-2 text-light text-decoration-none">Register</NavLink>
-            <img src="/logo192.png" style={{width:50}} alt="" />
+          
           </Nav>
         </Container>
       </Navbar>
